ea0e5d744e51a5007bed4f33350a442d270c4eba
maxoon <desfouxmaxime@gmail.com>
2018-09-06 15:27:09 +0200

    add : complete model

--- src/db/user.model.js
+++ src/db/user.model.js
@@ -1,10 +1,43 @@
 import mongoose, {Schema} from 'mongoose';
 
 const UserSchema = new Schema({
-    _id: { type: Number, default: Date.now },
-    titre: { type: String, required : true},
-    resume: { type: String, required : true},
-    img: { type: String, required : false},
+    user: {
+        _id: {type: Number, default: Date.now},
+        name: {type: String},
+        preferences: {
+            channels: {
+                title: {type: String},
+                img : {type: String},
+                _id: {type: Number, default: Date.now},
+                video : {type: String},
+                sub : {type: String},
+            },
+            myContents: {
+                title: {type: String},
+                img : {type: String},
+                _id: {type: Number, default: Date.now},
+                video : {type: String},
+                sub : {type: String},
+            },
+            apps: {
+                title: {type: String},
+                img : {type: String},
+                _id: {type: Number, default: Date.now},
+                url : {type: String},
+            },
+            films: {
+                title: {type: String},
+                img : {type: String},
+                _id: {type: Number, default: Date.now},
+                url : {type: String}
+            },
+            extras: {
+                title: {type: String},
+                img : {type: String},
+                _id: {type: Number, default: Date.now},
+            }
+        }
+    },
 });
 
 export const User = mongoose.model('User', UserSchema);
